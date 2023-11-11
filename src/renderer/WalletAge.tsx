8889ecd3da6b0877516aa22879cd4dc274dd1a85
笑恒 <xiaoheng.lj@alibaba-inc.com>
2023-11-11 15:59:07 +0800

    fix: 支持分批请求

--- src/renderer/WalletAge.tsx
+++ src/renderer/WalletAge.tsx
@@ -1,7 +1,13 @@
 import { useState } from 'react';
 
 import { Button, Card, Input, Radio } from 'antd';
-import { formatDate, getError, IData, sortDate } from './utils';
+import {
+  formatDate,
+  getError,
+  groupArrayElements,
+  IData,
+  sortDate,
+} from './utils';
 
 const { ipcRenderer } = window.electron;
 const { TextArea } = Input;
@@ -24,6 +30,39 @@ export default function Content() {
   const [loading, setLoading] = useState(false);
   const [data, setData] = useState<IData[]>([]);
   const [chain, setChain] = useState('eth');
+  const [inputValue, setInputValue] = useState('');
+
+  const handleReset = () => {
+    setShowResult(false);
+    setData([]);
+    setInputValue('');
+  };
+
+  const handleInput = (e: any) => {
+    setInputValue(e.target.value);
+    setData([]);
+    setShowResult(false);
+    const { value } = e.target;
+    if (!value) {
+      return;
+    }
+    const addresses = value.split('\n');
+    const d: IData[] = [];
+    addresses.forEach((item: any) => {
+      const a = item.trim();
+      if (isValidAddress(a)) {
+        d.push({
+          address: a,
+        });
+      } else {
+        d.push({
+          address: a,
+          error: '是非法地址',
+        });
+      }
+    });
+    setData(d);
+  };
 
   const handleClick = () => {
     if (loading) {
@@ -33,13 +72,36 @@ export default function Content() {
     const validData = data.filter((item) => !item.error);
     let max = validData.length;
     setShowResult(true);
+    // 全部非法的直接返回了
     if (max <= 0) {
       return;
     }
+    const { groups, loopCounts, batches } = groupArrayElements(validData, 10);
+
     setLoading(true);
-    validData.forEach((item) => {
-      ipcRenderer.sendMessage('get-wallet-age', item.address, chain);
-    });
+
+    // 当前批次的数量
+    let currentBacthCount = batches[0];
+    // 当前游标
+    let currentCursor = 0;
+    // 上一次游标
+    let lastCursor = -1;
+
+    // 轮询是否一批结束了，一批结束了可以请求下一批
+    const interval = setInterval(() => {
+      // 说明一批请求完了
+      if (currentCursor > lastCursor && currentCursor < loopCounts) {
+        lastCursor = currentCursor;
+        currentBacthCount = batches[currentCursor];
+        groups[currentCursor].forEach((item) => {
+          ipcRenderer.sendMessage('get-wallet-age', item.address, chain);
+        });
+      } else if (currentCursor >= loopCounts) {
+        // 结束轮询
+        clearInterval(interval);
+      }
+    }, 500);
+
     // @ts-ignore
     ipcRenderer.on('get-wallet-age', (dateStr: string) => {
       if (dateStr.startsWith('error')) {
@@ -58,6 +120,10 @@ export default function Content() {
       });
       setData(d);
 
+      currentBacthCount -= 1;
+      if (currentBacthCount === 0) {
+        currentCursor += 1;
+      }
       max -= 1;
       if (max === 0) {
         setLoading(false);
@@ -85,32 +151,18 @@ export default function Content() {
         rows={20}
         style={{ width: '100%', marginBottom: 12 }}
         placeholder="请输入合法钱包地址，多个换行"
-        onChange={(e) => {
-          setShowResult(false);
-          const { value } = e.target;
-          if (!value) {
-            return;
-          }
-          const addresses = value.split('\n');
-          const d: IData[] = [];
-          addresses.forEach((item) => {
-            const a = item.trim();
-            if (isValidAddress(a)) {
-              d.push({
-                address: a,
-              });
-            } else {
-              d.push({
-                address: a,
-                error: '是非法地址',
-              });
-            }
-          });
-          setData(d);
-        }}
+        value={inputValue}
+        onChange={handleInput}
       />
       <Button type="primary" onClick={handleClick} loading={loading}>
         一键获取钱包的年龄
+      </Button>
+      <Button
+        style={{ marginLeft: 12 }}
+        onClick={handleReset}
+        disabled={loading}
+      >
+        清空
       </Button>
       {showResult && (
         <p>
--- src/renderer/utils.ts
+++ src/renderer/utils.ts
@@ -1,4 +1,3 @@
-
 export interface IData {
   address?: string;
   age?: string;
@@ -7,7 +6,7 @@ export interface IData {
 
 export const getError = (str?: string) => {
   if (!str) {
-    return '地址可能出错'
+    return '地址可能出错';
   }
   if (str === 'null') {
     return '没有交易记录';
@@ -16,7 +15,7 @@ export const getError = (str?: string) => {
     return '获取数据超时，请尝试使用或更换代理';
   }
   return;
-}
+};
 
 export const formatDate = (str?: string) => {
   if (!str) {
@@ -29,11 +28,11 @@ export const formatDate = (str?: string) => {
   // let str = "from 711 days 8 hrs ago";
 
   // 从字符串中提取天和小时
-  let days = parseInt(str.split(" ")[1]);
-  let hours = parseInt(str.split(" ")[3]);
+  let days = parseInt(str.split(' ')[1]);
+  let hours = parseInt(str.split(' ')[3]);
 
   // 计算总毫秒数
-  let millisecs = (days * 24 * 60 * 60 * 1000) + (hours * 60 * 60 * 1000);
+  let millisecs = days * 24 * 60 * 60 * 1000 + hours * 60 * 60 * 1000;
 
   // 获取当前时间
   let now = new Date();
@@ -43,17 +42,17 @@ export const formatDate = (str?: string) => {
 
   // 提取年月日时分秒
   let year = pastDate.getFullYear();
-  let month = ("0" + (pastDate.getMonth() + 1)).slice(-2); // 因为月份是从0开始的，所以需要加一
-  let date = ("0" + pastDate.getDate()).slice(-2);
-  let hrs = ("0" + pastDate.getHours()).slice(-2);
-  let mins = ("0" + pastDate.getMinutes()).slice(-2);
-  let secs = ("0" + pastDate.getSeconds()).slice(-2);
+  let month = ('0' + (pastDate.getMonth() + 1)).slice(-2); // 因为月份是从0开始的，所以需要加一
+  let date = ('0' + pastDate.getDate()).slice(-2);
+  let hrs = ('0' + pastDate.getHours()).slice(-2);
+  let mins = ('0' + pastDate.getMinutes()).slice(-2);
+  let secs = ('0' + pastDate.getSeconds()).slice(-2);
 
   // 组合成 "YYYY-MM-MM HH:mm:ss" 格式
   let formattedDate = `${year}-${month}-${date} ${hrs}:${mins}:${secs}`;
 
   return formattedDate;
-}
+};
 
 export const sortDate = (arr: IData[]) => {
   // let arr = [
@@ -62,11 +61,31 @@ export const sortDate = (arr: IData[]) => {
   //   { age: '2021-05-20 10:10:10' },
   //   // 其他对象...
   // ];
-
-  return arr.sort(function(a: IData, b:IData) {
-    let dateA = new Date(a.age || 0), dateB = new Date(b.age || 0);
+  return arr.sort(function (a: IData, b: IData) {
+    let dateA = new Date(a.error ? 0 : a.age || 0),
+      dateB = new Date(b.error ? 0 : b.age || 0);
     // @ts-ignore
     return dateA - dateB; // 用dateA - dateB 是升序。如果你要降序的话，只要改成 dateB - dateA 即可。
   });
+};
 
+export function groupArrayElements(array: any[], groupSize: number = 30) {
+  let groups = [];
+  const batches = [];
+
+  for (let i = 0; i < array.length; i += groupSize) {
+    groups.push(array.slice(i, i + groupSize));
+  }
+
+  const loopCounts = groups.length;
+
+  for (let i = 0; i < groups.length; i++) {
+    batches.push(groups[i].length);
+  }
+
+  return {
+    groups,
+    loopCounts,
+    batches,
+  };
 }
